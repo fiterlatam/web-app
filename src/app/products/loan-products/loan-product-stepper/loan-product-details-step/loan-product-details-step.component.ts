78e4c6ad23fd01b9f86cbaa215471735229a89cf
Julius Peter Oketayot <julius@fiter.io>
2024-10-10 13:33:55 +0300

    Feature/SU-117: SU+ Pay Vehículos: cupo

--- src/app/products/loan-products/loan-product-stepper/loan-product-details-step/loan-product-details-step.component.ts
+++ src/app/products/loan-products/loan-product-stepper/loan-product-details-step/loan-product-details-step.component.ts
@@ -2,7 +2,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { Dates } from 'app/core/utils/dates';
-import { TooltipPosition } from '@angular/material/tooltip';
 
 /** Custom Services */
 import { SettingsService } from 'app/settings/settings.service';
@@ -25,7 +24,7 @@ export class LoanProductDetailsStepComponent implements OnInit {
   maxDate = new Date(new Date().setFullYear(new Date().getFullYear() + 10));
 
   /**
-   * @param {FormBuilder} formBuilder Form Builder.
+   * @param {UntypedFormBuilder} formBuilder Form Builder.
    * @param {Dates} dateUtils Date Utils.
    * @param {SettingsService} settingsService Settings Service.
    */
@@ -48,7 +47,16 @@ export class LoanProductDetailsStepComponent implements OnInit {
       'closeDate': this.loanProductsTemplate.closeDate && new Date(this.loanProductsTemplate.closeDate),
       'includeInBorrowerCycle': this.loanProductsTemplate.includeInBorrowerCycle,
       'advance': this.loanProductsTemplate.advance,
-      'productType': this.loanProductsTemplate.productType ? this.loanProductsTemplate.productType.id : ''
+      'productType': this.loanProductsTemplate.productType ? this.loanProductsTemplate.productType.id : '',
+      'vehicleCupo': this.loanProductsTemplate.vehicleCupo,
+    });
+    this.loanProductDetailsForm.get('productType')?.valueChanges.subscribe(productTypeValue => {
+        if (this.isProductTypeSuVehicle(productTypeValue)) {
+          this.loanProductDetailsForm.get('vehicleCupo')?.setValidators([Validators.required]);
+        } else {
+          this.loanProductDetailsForm.get('vehicleCupo')?.clearValidators();
+        }
+        this.loanProductDetailsForm.get('vehicleCupo')?.updateValueAndValidity();
     });
   }
 
@@ -62,7 +70,8 @@ export class LoanProductDetailsStepComponent implements OnInit {
       'closeDate': [''],
       'includeInBorrowerCycle': [false],
       'advance': [false],
-      'productType': ['', Validators.required]
+      'productType': ['', Validators.required],
+      'vehicleCupo': [''],
     });
   }
 
@@ -80,4 +89,14 @@ export class LoanProductDetailsStepComponent implements OnInit {
     return loanProductDetailsFormData;
   }
 
+  isProductTypeSuVehicle(productTypeValue?: string): boolean {
+    const productTypeId = productTypeValue ? productTypeValue : this.loanProductDetailsForm.value.productType;
+    for (let i = 0; i < this.productTypeData.length; i++) {
+      if (this.productTypeData[i].name === 'SU+ Vehiculos'
+        && this.productTypeData[i].id === productTypeId) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
