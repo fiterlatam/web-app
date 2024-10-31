f02f421fc3d6d0762351f4cec7de35c2dca6589a
Farooq <farooq@fiter.io>
2024-10-31 21:55:47 +0100

    [SU-402]Implementar Nota de Crédito

--- src/app/loans/loans-view/loan-account-actions/add-credit-note/add-credit-note.component.ts
+++ src/app/loans/loans-view/loan-account-actions/add-credit-note/add-credit-note.component.ts
@@ -38,7 +38,6 @@ export class AddCreditNoteComponent implements OnInit {
   }
 
   onFileSelected(event: Event): void {
-    console.log('File Selected:', event);
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
