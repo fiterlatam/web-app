98a6b52561ec3dbfd19c86171d4a65187c89d2d7
Daniel Stancampiano <dstanca@vwodonto,com.br>
2025-02-06 15:52:39 -0300

    feature/SU-580 - FE -Adjustments for Billing (Facturación) rango de numeración habilitada hasta

--- src/app/products/loan-product-parameterization/create-loan-product-parameterization/create-loan-product-parameterization.component.ts
+++ src/app/products/loan-product-parameterization/create-loan-product-parameterization/create-loan-product-parameterization.component.ts
@@ -49,15 +49,11 @@ export class CreateLoanProductParameterizationComponent implements OnInit {
       expirationDate: ['', Validators.required],
       rangeStartNumber: [0, [
         Validators.required,
-        Validators.maxLength(4),
-        Validators.pattern('^[0-9]{1,4}$'), // Ensures only digits, 1-4 characters
-        Validators.max(9999) // Ensures numeric value doesn't exceed 9999
+        Validators.max(9007199254740991) // Ensures numeric value doesn't exceed 9999
       ]],
       rangeEndNumber: [0, [
         Validators.required,
-        Validators.maxLength(4),
-        Validators.pattern('^[0-9]{1,4}$'), // Ensures only digits, 1-4 characters
-        Validators.max(9999) // Ensures numeric value doesn't exceed 9999
+        Validators.max(9007199254740991) // Ensures numeric value doesn't exceed 9999
       ]],
       lastInvoiceNumber: [0, Validators.required],
       lastCreditNoteNumber: [0, Validators.required],
